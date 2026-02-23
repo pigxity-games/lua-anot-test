020b735039c519ca28637bc681ac8bdf219ae3d5
pigxity <87405798+pigxity@users.noreply.github.com>
2026-02-22 23:26:22 -0500

    make components provide cleanup functions

--- src/client/Counter.lua
+++ src/client/Counter.lua
@@ -1,6 +1,7 @@
 --@component, [Counter]
 local component = {
-    count = 0
+    count = 0,
+    active = true
 }
 
 function component:_init(inst: BasePart)
@@ -8,10 +9,24 @@ function component:_init(inst: BasePart)
     local detector: ClickDetector = inst:WaitForChild("ClickDetector")
     local label: TextLabel = inst.SurfaceGui.TextLabel
 
-    detector.MouseClick:Connect(function(player)
+    self.conn = detector.MouseClick:Connect(function(player)
         self.count += 1
         label.Text = "Count: " .. self.count
     end)
+
+    task.spawn(function()
+        while self.active do
+            print("Counter: " .. self.instance.Name .. " count = " .. self.count)
+            task.wait(3)
+        end
+    end)
 end
+
+function component:_destroy()
+    print("Counter: Disconnect")
+    self.conn:Disconnect()
+    self.active = false
+end
+
 
 return component
--- src/server/KillBrick.lua
+++ src/server/KillBrick.lua
@@ -3,13 +3,17 @@ local module = {}
 
 --@bindTag, [KillBrick]
 function module.killBrick(inst: BasePart)
-    inst.Touched:Connect(function(p)
+    local conn = inst.Touched:Connect(function(p)
         local char = p.Parent
         local hum = char:FindFirstChild("Humanoid") :: Humanoid
         if hum then
             hum.Health = 0
         end
     end)
+    return function()
+        print("Killbrick: disconnect")
+        conn:Disconnect()
+    end
 end
 
 return module
